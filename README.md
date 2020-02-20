4679b5dbfd374ebb6d94541dbfb4df7c16356108
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-02-20 17:58:38 +0100

    feat(init): initialized project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# nocode-nobug
-No Code, No Bug. A blog with tech articles that might be helpful. Or not!
+# No Code, No Bug
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Footer from "components/Footer";
+
+let myVar: string = "42";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello, World! {myVar}</h1>
+      <Footer />
+    </div>
+  );
+};
+export default IndexPage;
