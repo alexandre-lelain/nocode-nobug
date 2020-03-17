e9505b0f50df65e16777b8645779e9f9bf434b59
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-03-17 09:57:21 +0100

    feat(accessibility): improve links color for better accessibility

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,7 @@
 const createTheme = (isDark: boolean): object => ({
   palette: {
     inlineCode: '#cce0ff',
+    links: isDark ? '#19b9d2' : '#0066cc',
     text: {
       primary: isDark ? 'rgba(255, 255, 255, 0.9)' : 'rgba(0, 0, 0, 0.87)',
     },
